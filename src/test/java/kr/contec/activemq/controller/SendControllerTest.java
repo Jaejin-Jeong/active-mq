27543c4c0104622bf921d3206465f4b8866ae5f2
jaejin <jaejin>
2022-04-14 14:27:47 +0900

    Add

---
+++ src/test/java/kr/contec/activemq/controller/SendControllerTest.java
@@ -0,0 +1,20 @@
+package kr.contec.activemq.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SendControllerTest {
+
+    @Test
+    void sendMessage() {
+    }
+
+    @Test
+    void sendQueuesMessage() {
+    }
+
+    @Test
+    void sendTopicMessage() {
+    }
+}
